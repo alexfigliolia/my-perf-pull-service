88750a529b6806306835b6569133a1222b087d36
Alex Figliolia <alexfigliolia@gmail.com>
2024-03-10 23:06:13 -0700

    Move to polling for cron jobs

--- src/Subscriptions/RepositoryPulls/RepositoryPulls.ts
+++ src/Subscriptions/RepositoryPulls/RepositoryPulls.ts
@@ -1,15 +1,10 @@
 import { BaseSubscription } from "@alexfigliolia/my-performance-async";
-import {
-  AsyncServiceRequest,
-  AsyncServiceSubscription,
-} from "@alexfigliolia/my-performance-clients";
-import { nextRepositoryPullJob, repositoryPulls } from "GQL";
+import { AsyncServiceRequest } from "@alexfigliolia/my-performance-clients";
+import { nextRepositoryPullJob } from "GQL";
 import {
   type NextRepositoryPullJobQuery,
   type NextRepositoryPullJobQueryVariables,
   Platform,
-  type RepositoryPullsSubscription,
-  type RepositoryPullsSubscriptionVariables,
 } from "GQL/AsyncService/Types";
 import { GithubRepositoryPull } from "Pulls";
 import type { Config, IncomingJob } from "./types";
@@ -18,24 +13,8 @@ export class RepositoryPulls extends BaseSubscription<
   Config,
   GithubRepositoryPull
 > {
-  public stream = new AsyncServiceSubscription<
-    RepositoryPullsSubscription,
-    RepositoryPullsSubscriptionVariables
-  >(repositoryPulls, {});
-
   public initialize() {
     void this.poll();
-    this.stream.open();
-    this.stream.onData(response => {
-      if (!response.data?.repositoryPulls) {
-        return;
-      }
-      const { repositoryPulls: next } = response.data;
-      if (next) {
-        const config = this.parseResponse(next);
-        this.onStream(config);
-      }
-    });
     return this;
   }
 
@@ -51,7 +30,7 @@ export class RepositoryPulls extends BaseSubscription<
       const config = this.parseResponse(response.data.nextRepositoryPullJob);
       this.onPoll(config);
     } catch (error) {
-      this.onPoll(null);
+      this.activatePollInterval();
     }
   }
 
